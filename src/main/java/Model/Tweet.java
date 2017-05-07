0dfdcfeabaea106443e0a8622c14fe3824ccdb12
MonodetH <monodeth@gmail.com>
2017-05-07 15:51:30 -0300

    Implementadas las clases Tweet y User

--- src/main/java/Model/Tweet.java
+++ src/main/java/Model/Tweet.java
@@ -1,30 +1,76 @@
 package Model;
 
+import twitter4j.HashtagEntity;
 import twitter4j.Status;
+import twitter4j.UserMentionEntity;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Tweet{
     // Twitter Status
-    int _id;
-    String createdAt;
-    String text;
-    int inReplyToStatusId;
-    int inReplyToUserId;
-    Tweet retweetedStatus;
-    Tweet QuotedStatus;
-    List<User> userMentions;
-    List<String> hashtags;
-    User user;
+    private long _id;
+    private Date createdAt;
+    private String text;
+    private long inReplyToStatusId;
+    private long inReplyToUserId;
+    private Tweet retweetedStatus;
+    private Tweet quotedStatus;
+    private User[] userMentions;
+    private String[] hashtags;
+    private  User user;
 
     // Politweets
-    String sentimiento;
-    boolean textIndexed;
-    boolean userIndexed;
-    boolean geoIndexed;
+    private String sentimiento = null;
+    private boolean textIndexed = false;
+    private boolean userIndexed = false;
+    private boolean geoIndexed = false;
 
-    public Tweet(Status status){
+     Tweet(Status s){
+        _id = s.getId();
+        createdAt = s.getCreatedAt();
+        text = s.getText();
+        inReplyToStatusId = s.getInReplyToStatusId();
+        inReplyToUserId = s.getInReplyToUserId();
+        retweetedStatus = new Tweet(s.getRetweetedStatus());
+        quotedStatus = new Tweet(s.getQuotedStatus());
+        user = new User(s.getUser());
 
+        UserMentionEntity[] menciones = s.getUserMentionEntities();
+        userMentions = new User[menciones.length];
+        for (int i=0;i<menciones.length;i++) {
+            userMentions[i] = new User(menciones[i]);
+        }
+
+        HashtagEntity[] statusHashtags = s.getHashtagEntities();
+        hashtags = new String[statusHashtags.length];
+        for (int i=0;i<statusHashtags.length;i++) {
+            hashtags[i] = statusHashtags[i].getText();
+        }
     }
+
+    // GETTERS
+    public long getId() { return _id;}
+    public String getText() {return text;}
+    public Date getCreatedAt() {return createdAt;}
+    public User getUser() {return user;}
+    public User[] getUserMentions() {return userMentions;}
+    public String[] getHashtags() {return hashtags;}
+    public long getInReplyToStatusId() { return inReplyToStatusId;}
+    public long getInReplyToUserId() {return inReplyToUserId;}
+    public Tweet getQuotedStatus() {return quotedStatus;}
+    public Tweet getRetweetedStatus() {return retweetedStatus;}
+    public String getSentimiento() {return sentimiento;}
+    public boolean isGeoIndexed() {return geoIndexed;}
+    public boolean isTextIndexed() {return textIndexed;}
+    public boolean isUserIndexed() {return userIndexed;}
+
+    // SETTERS
+    public void setSentimiento(String sentimiento) {this.sentimiento = sentimiento;}
+    public void setTextIndexed(boolean textIndexed) {this.textIndexed = textIndexed;}
+    public void setUserIndexed(boolean userIndexed) {this.userIndexed = userIndexed;}
+    public void setGeoIndexed(boolean geoIndexed) {this.geoIndexed = geoIndexed;}
+
 
 }
--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,12 +1,37 @@
 package Model;
 
+import twitter4j.UserMentionEntity;
+
 public class User {
     // Twitter
-    int _id;
-    String name;
-    String screenName;
-    int followersCount;
-    int friendsCount;
-    String location;
+    private long _id;
+    private String name;
+    private String screenName;
+    private int followersCount;
+    private int friendsCount;
+    private String location;
+
+    User(twitter4j.User u){
+        _id = u.getId();
+        name = u.getName();
+        screenName = u.getScreenName();
+        followersCount = u.getFollowersCount();
+        friendsCount = u.getFriendsCount();
+        location = u.getLocation();
+    }
+
+    User(UserMentionEntity m){
+        _id = m.getId();
+        name = m.getName();
+        screenName = m.getScreenName();
+    }
+
+    // GETTERS
+    public long get_id() {return _id;}
+    public String getName() {return name;}
+    public String getScreenName() {return screenName;}
+    public int getFollowersCount() {return followersCount;}
+    public int getFriendsCount() {return friendsCount;}
+    public String getLocation() {return location;}
 
 }
