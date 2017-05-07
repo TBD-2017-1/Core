a9db002a4e7827fd3418400547d92a5397982835
MonodetH <monodeth@gmail.com>
2017-05-06 21:12:55 -0300

    Initial commit

---
+++ src/main/java/Model/Tweet.java
@@ -0,0 +1,7 @@
+package Model;
+
+import twitter4j.Status;
+
+public abstract class Tweet extends Status {
+
+}
---
+++ src/main/java/MongoDBController.java
@@ -0,0 +1,56 @@
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
+import com.mongodb.util.JSON;
+import org.bson.Document;
+
+import com.google.gson.Gson;
+
+import twitter4j.Status;
+import twitter4j.json.DataObjectFactory;
+
+public class MongoDBController {
+	MongoClient mongoClient;
+	MongoDatabase db;
+	MongoCollection<Document> tweetsCollection;
+
+	public MongoDBController() {
+		mongoClient = new MongoClient("localhost", 27017);
+		db = mongoClient.getDatabase("politweets");
+		tweetsCollection = db.getCollection("tweets");
+	}
+
+
+
+	public void saveTweet(Status status){
+		Gson gson = new Gson();
+		String json = gson.toJson(obj);
+
+		BasicDBObject doc = (BasicDBObject) JSON.parse(json);
+		doc.append("")
+
+	}
+
+
+
+	public static DBObject tweetToDoc(Object obj){
+		Gson gson = new Gson();
+		// Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(obj);
+
+		BasicDBObject doc = (BasicDBObject) JSON.parse(json);
+
+		return doc;
+	}
+
+	public static Object docToDoc(DBObject doc, Object objClass){
+
+		Object obj =
+
+		return obj;
+	}
+}
